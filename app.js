4107803b677b8aaf416e38befef0eb7ec12e7552
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-08-19 11:26:57 +0200

    form data collected in event

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-function show_month() {
-  //
-}
+// UI Reference point
+let cardholderName = document.getElementById("cardholder-name");
+let cardholderNumber = document.getElementById("number");
+let cardMonth = document.getElementById("gMonth2");
+let cardYear = document.getElementById("year");
+let cardCvc = document.getElementById("cvc");
+
+// submit event
+
+document.querySelector("button").addEventListener("click", function (e) {
+  console.log(
+    "Name:",
+    cardholderName.value,
+    "Number:",
+    cardholderNumber.value,
+    "Month:",
+    cardMonth.value,
+    "Year:",
+    cardYear.value,
+    "CVC:",
+    cardCvc.value
+  );
+  e.preventDefault();
+});
